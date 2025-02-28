cad7f41479fe855c0de495c383fd40ec5869619a
Sunday Ige <sunnyaquostic@gmail.com>
2025-02-28 03:55:05 +0100

    add order deleting functionality for admin

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -81,15 +81,46 @@ export const updateOrderStatus = handleAsyncError(async (req, res, next) => {
         return next(new HandleError("This order has already been delivered", 404))
     }
 
-    await Promise.all(order.orderItems.map(item => console.log(item)))
+    await Promise.all(order.orderItems.map(item => updateQuantity(item.product, item.quantity)))
 
     order.orderStatus = req.body.status;
     if (order.orderStatus === 'Delivered'){
         order.deliveredAt = Date.now()
     }
 
+    await order.save({validateBeforeSave: false})
     res.status(200).json({
         success: true,
         order
+    })
+})
+
+async function updateQuantity(id, quantity) {
+    const product = await Product.findById(id);
+
+    if(!product) {
+        return next(new HandleError("Product not found!", 404))
+    }
+
+    product.stock -= quantity
+    await product.save({validateBeforeSave: false})
+}
+
+export const deleteOrder = handleAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if (!order) {
+        return next(new HandleError("No order found", 404))
+    }
+
+    if (order.orderStatus !== 'Delivered') {
+        return next(new HandleError("This order is under processing and can not be deleted!", 404))
+    }
+
+    await Order.deleteOne({_id: req.params.id})
+
+    res.status(200).json({
+        success: true,
+        message: "Order deleted successfully"
     })
 })
