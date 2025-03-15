aba6b4533c8d11d54a8370ce8a00a8f9bbfc144d
Sunday Ige <sunnyaquostic@gmail.com>
2025-03-15 17:05:48 +0100

    display product in home page

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import '../componentStyles/Navbar.css';
+import {Link} from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Navbar() {
+    const [isMenusOpened, setIsMenuOpened] = useState(false)
+    const toggleMenu = () => setIsMenuOpened(!isMenusOpened)
+    const isAuthenticated = true;
+
+  return (
+    <nav className="navbar">
+        <div className="navbar-container">
+            <div className="navbar-logo">
+                <Link to='/' onClick={()=>setIsMenuOpened(false)}>E-commerce</Link>
+            </div>
+
+            <div className={`navbar-links ${isMenusOpened ? 'active': ''}`}>
+                <ul>
+                    <li  onClick={()=>setIsMenuOpened(false)}><Link to='/' >Home</Link></li>
+                    <li><Link to='/products'>Products</Link></li>
+                    <li><Link to='/about-us'>About Us</Link></li>
+                    <li><Link to='/contact-us'>Contact Us</Link></li>
+                </ul>
+            </div>
+
+            <div className="navbar-icons">
+                <div className="search-container">
+                    <form className='search-form'>
+                        {/* <input 
+                            type="text" 
+                            className='search-input'
+                            placeholder='Search Products..'
+                        /> */}
+                        <button className="search-icon">
+                            <SearchIcon focusable='false' />
+                        </button>
+                    </form>
+                </div>
+
+                <div className="cart-container">
+                    <Link to='/cart'>
+                        <ShoppingCartIcon className="icon" />
+                        <span className="cart-badge">6</span>
+                    </Link>
+                </div>
+
+                {
+                    !isAuthenticated && <Link to='/register' className='register-link'>
+                        <PersonAddIcon className='icon' />
+                    </Link>
+                }
+                <div className="navbar-hamburger" onClick={toggleMenu}>
+                    {isMenusOpened ? <CloseIcon className='icon' /> : <MenuIcon className='icon'/>}
+                </div>
+
+
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,95 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar';
 import '../pageStyles/Home.css';
+import ImageSlider from '../components/ImageSlider';
+import Product from '../components/Product';
+
+const products = [
+  {
+      "_id": "67815149972a5bc7f151b78d",
+      "name": "product two",
+      "description": "Product description for two",
+      "price": 200,
+      "ratings": 0,
+      "image": [
+          {
+              "public_id": "This is the test id for second product",
+              "url": "this is the url for second product",
+              "_id": "67815149972a5bc7f151b78e"
+          }
+      ],
+      "category": "shirt",
+      "stock": 1,
+      "numberOfReviews": 1,
+      "reviews": [
+          {
+              "user": "67acd120a8674e5d1ee37bed",
+              "name": "Sunday Ige",
+              "rating": 6,
+              "comment": "nice product",
+              "_id": "67bb37ede290d264be35def8"
+          }
+      ],
+      "createdAt": "2025-01-10T16:56:41.112Z",
+      "__v": 1
+  },
+  {
+      "_id": "6783b353d22d4c1b64bb3fac",
+      "name": "product three",
+      "description": "Product description for three",
+      "price": 200,
+      "ratings": 0,
+      "image": [
+          {
+              "public_id": "This is the test id for third product",
+              "url": "this is the url for third product",
+              "_id": "6783b353d22d4c1b64bb3fad"
+          }
+      ],
+      "category": "shirt",
+      "stock": 1,
+      "numberOfReviews": 0,
+      "reviews": [],
+      "createdAt": "2025-01-12T12:19:31.199Z",
+      "__v": 0
+  },
+  {
+      "_id": "6783b3bcd22d4c1b64bb3faf",
+      "name": "product four",
+      "description": "Product description for four",
+      "price": 200,
+      "ratings": 0,
+      "image": [
+          {
+              "public_id": "This is the test id for four product",
+              "url": "this is the url for four product",
+              "_id": "6783b3bcd22d4c1b64bb3fb0"
+          }
+      ],
+      "category": "shirt",
+      "stock": 1,
+      "numberOfReviews": 0,
+      "reviews": [],
+      "createdAt": "2025-01-12T12:21:16.595Z",
+      "__v": 0
+  }
+]
 
 function Home() {
   return (
     <>
+      <Navbar />
+      <ImageSlider />
       <div className="home-container">
         <h2 className="home-heading">Treading Now</h2>
+        <div className="home-product-container">
+          {
+            products.map((product, index) => (
+              <Product product={product} key={index}/>
+            ))
+          }
+        </div>
       </div>
       <Footer />
     </>
