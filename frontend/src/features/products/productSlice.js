dd3268ea3464bcb64d56b7a77186ae4e47dace12
Sunday Ige <sunnyaquostic@gmail.com>
2025-03-20 19:02:34 +0100

    add get single product from frontend functionality

--- frontend/src/features/products/productSlice.js
+++ frontend/src/features/products/productSlice.js
@@ -13,13 +13,26 @@ export const getProduct =createAsyncThunk('product/getProduct', async (_, {rejec
         return rejectWithValue(error.response?.data || "An error occured")
     }
 })
+
+export const getProductDetails =createAsyncThunk('product/getProductDetails', async (id, {rejectWithValue}) => {
+    try {
+        const link = `/api/v1/product/${id}`
+        const { data } = await axios.get(link)
+
+        return data
+    } catch (error) {
+        return rejectWithValue(error.response?.data || "An error occured")
+    }
+})
+
 const productSlice = createSlice({
     name: 'product',
     initialState : {
         products: [],
         productCount: 0, 
         loading: false,
-        error: null
+        error: null,
+        product:null
     },
     reducers : {
         removeErrors : (state) => {
@@ -41,6 +54,21 @@ const productSlice = createSlice({
         .addCase(getProduct.rejected, (state, action) => {
             state.loading = false
             state.error    = action.payload || 'Something went wrong'
+        })
+
+        builder.addCase(getProductDetails.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(getProductDetails.fulfilled, (state, action) => {
+            console.log('Fulfilled action payload', action.payload)
+            state.loading = false
+            state.error = null;
+            state.product = action.payload.product
+        })
+        .addCase(getProductDetails.rejected, (state, action) => {
+            state.loading = false
+            state.error    = action.payload || 'Something went wrong' 
         })
     }
 })
