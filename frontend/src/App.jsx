dd70958abf952598412957d806851d203c280b3f
Sunday Ige <sunnyaquostic@gmail.com>
2025-03-30 18:05:12 +0100

    add single product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import ProductDetails from './pages/ProductDetails'
+import Products from './pages/Products'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   )
