300aecca1979b193bca9c127dfa594ec1db768c5
Sunday Ige <sunnyaquostic@gmail.com>
2025-04-19 14:14:32 +0100

    add reset password functionality to the application

--- frontend/src/User/ResetPassword.jsx
+++ frontend/src/User/ResetPassword.jsx
@@ -1,20 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../UserStyles/Form.css'
 import PageTitle from '../components/PageTitle'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeErrors, removeSuccess, resetPassword } from '../features/user/userSlice'
+import { toast } from 'react-toastify'
 
 function ResetPassword() {
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = ("")
     const { token } = useParams()
-    
+    const {success, loading, error} = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const navigate = useNavigate();
     const resetPasswordSubmit = (e) => {
         e.preventDefault()
         const data = {
             password,
             confirmPassword
         }
+        dispatch(resetPassword({token, userData:data}))
     }
+
+    useEffect(() => {
+    if (error) {
+        toast.error(error.message, {position:'top-center', autoClose:3000})
+        dispatch(removeErrors())
+    }
+    },[dispatch, error])
+
+    useEffect(() => {
+    if (success) {
+        toast.success('Password reset successfully', {position:'top-center', autoClose:3000})
+        dispatch(removeSuccess())
+        navigate('/login')
+    }
+    },[dispatch, success])
 
   return (
     <>
